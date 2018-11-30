a0434ad2ec8158ca66775c08457330efd972aec0
Emmanuel Ogbewe <ed_ehiorobo1997@yahoo.com>
2018-11-29 22:07:49 -0500

    Initial Commit

---
+++ CamR1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CamR1
+//
+//  Created by Emmanuel  Ogbewe on 11/29/18.
+//  Copyright © 2018 Emmanuel Ogbewe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
